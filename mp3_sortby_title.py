8fbc3592e8508ef886afcaae64eb53fba4cba5d7
Ari Schoenfeld <32694252+ari-schoenfeld@users.noreply.github.com>
2019-09-12 21:22:32 -0700

    Create mp3_sortby_title.py

---
+++ mp3_sortby_title.py
@@ -0,0 +1,31 @@
+# Imports
+from eyed3 import id3
+import os
+
+
+def get_mp3_files(directory):
+    return [os.path.join(directory,x) for x in os.listdir(directory) if '.mp3' in x and os.path.isfile(os.path.join(directory,x))]
+
+
+def main():
+    directory = r'D:\Music\Soundtrack\Assorted Anime Songs'
+    mp3_files = get_mp3_files(directory)
+    mp3_tuples = []
+
+    for mp3 in mp3_files:
+        tag = id3.Tag()
+        tag.parse(mp3)
+        mp3_tuples.append((mp3, tag.title))
+    
+    sorted_mp3_tuples = sorted(mp3_tuples, key = lambda x: x[1].lower())
+    counter = 1
+    for mp3_path, mp3_title in sorted_mp3_tuples:
+        tag = id3.Tag()
+        tag.parse(mp3_path)
+        tag.track_num = counter
+        tag.save()
+        counter += 1
+    
+
+if __name__ == "__main__":
+    main()
